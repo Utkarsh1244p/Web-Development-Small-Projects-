58b2f94c44d6008a591dcafe2bbf94c62698974d
Utkarsh Gayguwal <utkarshgayguwal123@gmail.com>
2024-07-29 19:23:27 +0530

    Practice Function Terms

---
+++ setting-&-getting-values-localStorage/index.js
@@ -0,0 +1,39 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+// Get the leads from the localStorage - PS: JSON.parse()
+// localStorage.getItem("myLeads")
+
+// Store it in a variable, leadsFromLocalStorage
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+// Log out the variable
+console.log(leadsFromLocalStorage)
+
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    //Setting myLeads in LocalStorage
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
+    
+    // To verify that it works:
+    console.log( localStorage.getItem("myLeads") )
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
